bd395d67615a4f621dc3d51c65130548f6cc4766
cxz66666 <1092889706@qq.com>
2022-11-25 13:36:24 +0800

    test: add simple test for microbench

--- cn/app/simple_test/simple_test.cpp
+++ cn/app/simple_test/simple_test.cpp
@@ -19,34 +19,31 @@ int main()
     ctx->connect_session(rmem::get_uri_for_process(1), 0);
     ctx2->connect_session(rmem::get_uri_for_process(1), 1);
 
-    usleep(100000);
-
-    unsigned long raddr1 = ctx->rmem_alloc(GB(1), rmem::VM_FLAG_READ | rmem::VM_FLAG_WRITE);
+    unsigned long raddr1 = ctx->rmem_alloc(GB(8), rmem::VM_FLAG_READ | rmem::VM_FLAG_WRITE);
 
     std::cout << "raddr: " << raddr1 << std::endl;
 
-    for (size_t i = 0; i < GB(1); i += PAGE_SIZE)
+    for (size_t i = 0; i < GB(8); i += PAGE_SIZE)
     {
         ctx->rmem_write_sync(object, i + raddr1, 10);
     }
-    usleep(500000);
-    unsigned long fork_addr1 = ctx->rmem_fork(raddr1, GB(1) / 2);
+    unsigned long fork_addr1 = ctx->rmem_fork(raddr1, GB(8) / 2);
     std::cout << "fork_addr1: " << fork_addr1 << std::endl;
 
     unsigned long join_addr1 = ctx2->rmem_join(fork_addr1, 0, ctx->concurrent_store_->get_remote_session_num());
     std::cout << "join_addr1: " << join_addr1 << " remote session" << ctx->concurrent_store_->get_remote_session_num() << std::endl;
 
-    for (size_t i = 0; i < GB(1) / 2; i += PAGE_SIZE)
+    for (size_t i = 0; i < GB(8) / 2; i += PAGE_SIZE)
     {
         ctx2->rmem_write_sync(object2, i + join_addr1, 10);
     }
-    for (size_t i = 0; i < GB(1); i += PAGE_SIZE)
+    for (size_t i = 0; i < GB(8); i += PAGE_SIZE)
     {
         ctx->rmem_read_sync(object, i + raddr1, 10);
         std::cout << static_cast<char *>(object) << std::endl;
     }
 
-    for (size_t i = 0; i < GB(1) / 2; i += PAGE_SIZE)
+    for (size_t i = 0; i < GB(8) / 2; i += PAGE_SIZE)
     {
         ctx2->rmem_read_sync(object2, i + join_addr1, 10);
         std::cout << static_cast<char *>(object2) << std::endl;
@@ -55,8 +52,8 @@ int main()
 
     std::cout << ctx2->rmem_free_msg_buffer(object2) << std::endl;
     std::cout << ctx->rmem_free_msg_buffer(object) << std::endl;
-    ctx->rmem_free(raddr1, GB(1));
-    ctx2->rmem_free(join_addr1, GB(1) / 2);
+    ctx->rmem_free(raddr1, GB(8));
+    ctx2->rmem_free(join_addr1, GB(8) / 2);
     ctx->disconnect_session();
     ctx2->disconnect_session();
     delete ctx;
